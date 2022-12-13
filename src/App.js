2e14598ab91d96a4769fadb69bc78f46a4356ef0
manasa <athallurmanasa33@gmail.com>
2022-12-13 00:35:42 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+
+  <h1> welcome</h1>
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Welcome to </h1>
+     <h2> Hello React</h2>
+      
+      <SampleItem Fullname></SampleItem>
+      <SampleItem></SampleItem>
+
+
+      
+      
     </div>
   );
-}
+
 
 export default App;
+
+
+
