0391319bb9ed34a595ac8268cea1d54b9ab33d91
manasa <athallurmanasa33@gmail.com>
2022-12-13 23:07:14 -0600

    first commit

---
+++ src/test.ts
@@ -0,0 +1,22 @@
+import './App.css';
+
+import  AddUser from './components/users/AddUser.js';
+import Card from './components/users/Card.js';
+
+
+function App() {
+  return ( <div>
+
+<AddUser> </AddUser>
+ 
+ <Card>
+  
+<h1> Hello everyone</h1>
+
+</Card>
+  </div>  );
+  
+          
+}
+
+export default App;
